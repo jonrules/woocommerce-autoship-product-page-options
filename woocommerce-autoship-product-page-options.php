e1272bc4fb5ad5514ed4fe9d039cc0a25e231281
Jonathan Baltazar <jonrules@gmail.com>
2015-09-15 18:05:36 -0500

    update version number to 1.0.2

--- woocommerce-autoship-product-page-options.php
+++ woocommerce-autoship-product-page-options.php
@@ -3,13 +3,13 @@
 Plugin Name: WC Autoship Product Page Options
 Plugin URI: http://wooautoship.com
 Description: Customize the autoship options on the product page.
-Version: 1.0.1
+Version: 1.0.2
 Author: Patterns in the Cloud
 Author URI: http://patternsinthecloud.com
 License: Single-site
 */
 
-define( 'WC_Autoship_Product_Page_Options_Version', '1.0.1' );
+define( 'WC_Autoship_Product_Page_Options_Version', '1.0.2' );
 
 include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 if ( is_plugin_active( 'woocommerce-autoship/woocommerce-autoship.php' ) ) {
